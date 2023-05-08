84df15c7e3dece09e141ecef954d4ef913b5e7fd
jackbur73 <111925603+jackbur73@users.noreply.github.com>
2023-05-08 16:01:37 -0500

    Update and rename twos_comp.vhd to twosComp.vhd

--- twosComp.vhd
+++ twosComp.vhd
@@ -31,7 +31,7 @@ begin
 
   U1: NOT2 port map(a(0) , outTemp(0));
    U2: NOT2 port map(a(1) , outTemp(1));
-    U3: NOT2 port map(a(2) , outTemp(2));
+    U3: NOT2 port map(a(2) , outTemp(2)); -- 2s Comp is 8 nots with 1 added to the end
      U4: NOT2 port map(a(3) , outTemp(3));
       U5: NOT2 port map(a(4) , outTemp(4));
        U6: NOT2 port map(a(5) , outTemp(5));
@@ -39,7 +39,7 @@ begin
          U8: NOT2 port map(a(7) , outTemp(7));
 
 U9 : FullAdder port map ( outTemp(0) , '1' , '0' , twoscomp(0) , Cout(0));
-U10 : FullAdder port map ( outTemp(1) ,'0', Cout(0) , twoscomp(1) , Cout(1));
+U10 : FullAdder port map ( outTemp(1) ,'0', Cout(0) , twoscomp(1) , Cout(1)); -- Nots with FAdder
 U11 : FullAdder port map ( outTemp(2) , '0' , Cout(1) , twoscomp(2) , Cout(2));
 U12 : FullAdder port map ( outTemp(3) , '0' , Cout(2) , twoscomp(3) , Cout(3));
 U13 : FullAdder port map ( outTemp(4) , '0' , Cout(3) , twoscomp(4) , Cout(4));
